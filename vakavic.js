4b9e3a66a4f47e944d6c85ee9cdeba9cf4348229
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2019-09-05 13:55:31 +0430

    replace callback with promise

--- vakavic.js
+++ vakavic.js
@@ -1,30 +1,29 @@
-var request = require('request');
-
+var request = require('request-promise-native');
 
 module.exports = class Vakavic {
-
   constructor(options) {
-
-    if (options.api_token == undefined){
-      throw new Error('Vakavic API_TOKEN not initilized please log in to your vakavic account to obtain an API_TOKEN');
+    if (options.api_token == undefined) {
+      throw new Error(
+        'Vakavic API_TOKEN not initilized please log in to your vakavic account to obtain an API_TOKEN'
+      );
     }
 
     this.api_token = options.api_token;
-    this.api_url = "https://api.vakavic.com";
+    this.api_url = 'https://api.vakavic.com';
     this.header = {
-      'content-type' : 'application/json',
-      'Authorization': 'Bearer '+this.api_token
-    }
+      'content-type': 'application/json',
+      Authorization: 'Bearer ' + this.api_token
+    };
 
     this.sentenceCount = 5;
 
-    if (options.sentenceCount != undefined){
+    if (options.sentenceCount != undefined) {
       this.sentenceCount = options.sentenceCount;
     }
   }
 
-  classify(module_key, text, done){
-    let url = this.api_url + "/classifier/classify";
+  classify(module_key, text) {
+    let url = this.api_url + '/classifier/classify';
 
     var options = {
       headers: this.header,
@@ -37,18 +36,11 @@ module.exports = class Vakavic {
       }
     };
 
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        done(null, body);
-      }
-      else{
-        done(error)
-      }
-    });
+    return request(options);
   }
 
-  getClassifications(module_key, text, done){
-    let url = this.api_url + "/classifier/getClassifications";
+  getClassifications(module_key, text) {
+    let url = this.api_url + '/classifier/getClassifications';
 
     var options = {
       headers: this.header,
@@ -61,18 +53,11 @@ module.exports = class Vakavic {
       }
     };
 
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        done(null, body);
-      }
-      else{
-        done(error)
-      }
-    });
+    return request(options);
   }
 
-  batchClassifications(module_key, text, done){
-    let url = this.api_url + "/classifier/batchClassifications";
+  batchClassifications(module_key, data) {
+    let url = this.api_url + '/classifier/batchClassification';
 
     var options = {
       headers: this.header,
@@ -81,22 +66,15 @@ module.exports = class Vakavic {
       rejectUnauthorized: false,
       json: {
         referenceKey: module_key,
-        text: text
+        data: data
       }
     };
 
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        done(null, body);
-      }
-      else{
-        done(error)
-      }
-    });
+    return request(options);
   }
 
-  summarizeText(text, done, sentenceCount = null){
-    let url = this.api_url + "/summarizer/summarizetext";
+  summarizeText(text, sentenceCount = null) {
+    let url = this.api_url + '/summarizer/summarizetext';
 
     var options = {
       headers: this.header,
@@ -105,18 +83,11 @@ module.exports = class Vakavic {
       rejectUnauthorized: false,
       json: {
         text: text,
-        sentenceCount: sentenceCount == null ? this.sentenceCount : sentenceCount
+        sentenceCount:
+          sentenceCount == null ? this.sentenceCount : sentenceCount
       }
     };
 
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        done(null, body);
-      }
-      else{
-        done(error)
-      }
-    });
+    return request(options);
   }
-
-}
+};
